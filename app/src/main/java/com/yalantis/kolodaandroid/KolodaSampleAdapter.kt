356de68f4542966e069ebbe62102bf67d8466349
pradipawasthi <pradipawasthi701@gmail.com>
2018-09-29 21:40:58 +0530

    latest

--- app/src/main/java/com/yalantis/kolodaandroid/KolodaSampleAdapter.kt
+++ app/src/main/java/com/yalantis/kolodaandroid/KolodaSampleAdapter.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_koloda.view.*
 
 
@@ -65,9 +68,10 @@ class KolodaSampleAdapter(val context: Context,val data: List<Int>?) : BaseAdapt
        private val picture = view.kolodaImage
 
         internal fun bindData(context: Context, data: Int) {
-//            val transforms = RequestOptions().transforms(CenterCrop(), RoundedCorners(20))
+            val transforms = RequestOptions().transforms(CenterCrop(), RoundedCorners(20))
             Glide.with(context)
                     .load(data)
+                    .apply(transforms)
                     .into(picture)
         }
 
